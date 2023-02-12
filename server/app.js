3e27ea19af6ce7e74a742133c9c21d339e69333d
skumar61 <skumar61@paypal.com>
2023-02-12 14:10:38 +0530

    create user and its profile

--- server/app.js
+++ server/app.js
@@ -1,41 +1,83 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var firebase = require("./firebase/firebase");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./config/cwdr-application-firebase-adminsdk-cifbw-3581e1d5ec.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+let uid;
+admin
+  .auth()
+  .createUser({
+    email: "user12@example.com",
+    emailVerified: false,
+    password: "secretPassword",
+    displayName: "John Doe",
+    photoURL: "http://www.example.com/12345678/photo.png",
+    disabled: false,
+  })
+  .then(async (userRecord) => {
+    // See the UserRecord reference doc for the contents of userRecord.
+    console.log("Successfully created new user:", userRecord.uid);
+
+    await firebase.createUserProfileDocument(userRecord);
+
+    uid = userRecord.uid;
+    console.log(uid);
+    admin
+      .auth()
+      .getUser(uid)
+      .then((userRecord) => {
+        // See the UserRecord reference doc for the contents of userRecord.
+        console.log(`Successfully fetched user data: ${userRecord.toJSON()}`);
+      })
+      .catch((error) => {
+        console.log("Error fetching user data:", error);
+      });
+  })
+  .catch((error) => {
+    console.log("Error creating new user:", error);
+  });
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- server/firebase/firebase.js
+++ server/firebase/firebase.js
@@ -1,7 +1,46 @@
-var admin = require("firebase-admin");
+var appn = require("firebase/app");
+var authn = require("firebase/auth");
+var firestore = require("firebase/firestore");
 
-var serviceAccount = require("../config/cwdr-application-firebase-adminsdk-cifbw-3581e1d5ec.json");
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBSMb3F7h2dzo638L_pEvwTfHWH_ucJAXA",
+  authDomain: "cwdr-application.firebaseapp.com",
+  projectId: "cwdr-application",
+  storageBucket: "cwdr-application.appspot.com",
+  messagingSenderId: "202195066477",
+  appId: "1:202195066477:web:fccc7b6e0e1002c810e964",
+};
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+// Initialize Firebase
+const app = appn.initializeApp(firebaseConfig);
+
+const auth = authn.getAuth(app);
+const db = firestore.getFirestore(app);
+
+const createUserProfileDocument = async (userAuth) => {
+  if (!userAuth) return;
+
+  console.log("creating profile");
+  console.log(userAuth.uid);
+  const userRef = firestore.doc(db, "user", userAuth.uid);
+
+  const userSnapshot = await firestore.getDoc(userRef);
+
+  console.log(userSnapshot.data());
+  if (!userSnapshot.exists()) {
+    console.log("does not exists");
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    firestore
+      .setDoc(userRef, { displayName, createdAt, email, role: ["admin"] })
+      .catch((err) => console.log(err));
+  }
+  return userRef;
+};
+
+module.exports = {
+  createUserProfileDocument,
+};
