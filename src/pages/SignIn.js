8bdff6007c47f4b2b78eaea09d016b75b490d5f8
suraj kumar <skumar61@paypal.com>
2023-03-02 07:21:45 +0530

    signin

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,31 +1,31 @@
-
-
-import React, { useState } from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import CircularProgress from '@mui/material/CircularProgress';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { green } from '@mui/material/colors';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import React, { useState } from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import CircularProgress from "@mui/material/CircularProgress";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { green } from "@mui/material/colors";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useNavigate } from "react-router-dom";
 
 import { Header } from "../components";
-import { useAuth } from '../hooks';
+import { useAuth } from "../hooks";
 
 const theme = createTheme();
 
 export const SignIn = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [loading, setLoading] = React.useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  const { signInWithEmail } = useAuth();
+  const navigate = useNavigate();
+  const { currentUser, signInWithEmail } = useAuth();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -34,32 +34,39 @@ export const SignIn = () => {
     }
 
     await signInWithEmail(email, password);
-    setEmail('');
-    setPassword('')
+    setEmail("");
+    setPassword("");
     setLoading(false);
   };
 
-  return (
+  const comp = currentUser ? (
+    navigate("/")
+  ) : (
     <ThemeProvider theme={theme}>
       <Container component="main" maxWidth="xs">
         <CssBaseline />
         <Box
           sx={{
             marginTop: 16,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
           <Header drawerWidth={0} />
 
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
             <TextField
               value={email}
               onChange={(e) => setEmail(e.target.value)}
@@ -93,18 +100,18 @@ export const SignIn = () => {
               Sign In
             </Button>
             {loading && (
-            <CircularProgress
-              size={25}
-              sx={{
-                color: green[500],
-                position: 'absolute',
-                top: '50%',
-                left: '50%',
-                marginTop: '-12px',
-                marginLeft: '-12px',
-              }}
-            />
-          )}
+              <CircularProgress
+                size={25}
+                sx={{
+                  color: green[500],
+                  position: "absolute",
+                  top: "50%",
+                  left: "50%",
+                  marginTop: "-12px",
+                  marginLeft: "-12px",
+                }}
+              />
+            )}
             <Grid container>
               <Grid item xs>
                 <Link href="#" variant="body2">
@@ -117,4 +124,6 @@ export const SignIn = () => {
       </Container>
     </ThemeProvider>
   );
-}
+
+  return comp;
+};
