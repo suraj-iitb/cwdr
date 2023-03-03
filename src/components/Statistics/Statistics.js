825de63d3b7588c753fef71b9eb0c0ddac53bd7a
suraj kumar <skumar61@paypal.com>
2023-03-03 10:12:44 +0530

    stats

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,21 +1,70 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import { PieChart, Pie, ResponsiveContainer } from 'recharts';
-
-const data01 = [
-    { name: 'Manushi', value: 400 },
-    { name: 'Mythri', value: 300 },
-    { name: 'Snehdi', value: 200 },
-  ];
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import { PieChart, Pie, ResponsiveContainer, Tooltip } from "recharts";
+import { useState, useRef, useEffect } from "react";
+import { retrieveDocsCount } from "../../firebase";
+import { COLLECTIONS } from "../../constants/constants";
+import { Typography } from "@mui/material";
 
 export function Statistics() {
+  const [data01, setData01] = React.useState([]);
+
+  useEffect(() => {
+    const cnt = async () => {
+      console.log("in count");
+
+      const maithriCount = await retrieveDocsCount("mythri");
+      const manushiCount = await retrieveDocsCount("manushi");
+      const snehidhiCount = await retrieveDocsCount("snehidi");
+
+      setData01([
+        { name: "Manushi", value: manushiCount },
+        { name: "Mythri", value: maithriCount },
+        { name: "Snehdi", value: snehidhiCount },
+      ]);
+      console.log(data01);
+      console.log("out count");
+    };
+    cnt();
+    // console.log(data01)
+  }, []);
+
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+    index,
+  }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+      <text
+        x={x}
+        y={y}
+        fill="white"
+        textAnchor={x > cx ? "start" : "end"}
+        dominantBaseline="central"
+      >
+        {`${data01[index].name}: ${data01[index].value}`}
+
+        {/* {`${(percent * 100).toFixed(0)}%`} */}
+      </text>
+    );
+  };
+
   return (
     <Box
       sx={{
-        display: 'flex',
-        flexWrap: 'wrap',
-        '& > :not(style)': {
+        display: "flex",
+        flexWrap: "wrap",
+        "& > :not(style)": {
           m: 1,
           width: 500,
           height: 500,
@@ -24,10 +73,23 @@ export function Statistics() {
     >
       <Paper elevation={0}>
         <ResponsiveContainer width="100%" height="100%">
-            <PieChart width={400} height={400}>
-                <Pie data={data01} dataKey="value" cx="50%" cy="50%" fill="#82ca9d" label />
-            </PieChart>
+          <PieChart width={400} height={400}>
+          <text x={500 / 2} y={20} fill="black" textAnchor="middle" dominantBaseline="central">
+            <tspan fontSize="14">No of Users</tspan>
+        </text>
+            <Pie
+              data={data01}
+              dataKey="value"
+              cx="50%"
+              cy="50%"
+              fill="#82ca9d"
+              labelLine={false}
+            label={renderCustomizedLabel}
+            />
+            <Tooltip />
+          </PieChart>
         </ResponsiveContainer>
+
       </Paper>
     </Box>
   );
--- src/firebase/utils.js
+++ src/firebase/utils.js
@@ -1,14 +1,25 @@
-import { doc, getDoc } from "firebase/firestore";
+import {
+  doc,
+  getDoc,
+  collection,
+  getCountFromServer,
+} from "firebase/firestore";
 
-import { db } from './firebase';
+import { db } from "./firebase";
 
-export const retrieveDoc = async (collection, id) => {
-    const docRef = doc(db, collection, id);
-    const docSnap = await getDoc(docRef);
+export const retrieveDoc = async (collectionName, id) => {
+  const docRef = doc(db, collectionName, id);
+  const docSnap = await getDoc(docRef);
 
-    if (docSnap.exists()) {
-        return docSnap.data();
-    } else {
-        return {error: "No such document!"};
-    }
-}
+  if (docSnap.exists()) {
+    return docSnap.data();
+  } else {
+    return { error: "No such document!" };
+  }
+};
+
+export const retrieveDocsCount = async (collectionName) => {
+  const collectionRef = collection(db, collectionName);
+  const snapshot = await getCountFromServer(collectionRef);
+  return snapshot.data().count;
+};
